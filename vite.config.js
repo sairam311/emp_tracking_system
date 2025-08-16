553f9072900ae079ce1c2e33e9c92f6315e1951e
sairam311 <kaminenisairam33@gmail.com>
2025-08-17 00:45:22 +0530

    working demo

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Employee_Tracker/'
+  base: '/emp_tracking_system/'
 })
